5fd39f34f84026e8450efb466d44cd2b9d171273
mawhal <mawhal@gmail.com>
2024-08-05 13:45:44 -0400

    update SEM code to more easily share analysis

--- R/sem.R
+++ R/sem.R
@@ -14,20 +14,26 @@ library(tidyverse)
 library(lavaan)
 library(piecewiseSEM)
 
-# prepare data
-# read wide data from script "R/lagged_regression.R"
-dwide <- read_csv("data/data_wide.csv")  
-# rename sites
-dwide$site <- unlist( lapply( strsplit(dwide$site,"-"), function(z) z[2] ) )
-# remove NA values for relevant variables
-dpick = dwide %>% filter( ! is.na(logrug_90), ! is.na(total_cover_90)  )
+# # code chunk below prepare data from other scripts in the project ----- 
+# # read wide data from script "R/lagged_regression.R"
+# dwide <- read_csv("data/data_wide.csv")  
+# # rename sites
+# dwide$site <- unlist( lapply( strsplit(dwide$site,"-"), function(z) z[2] ) )
+# # remove NA values for relevant variables
+# dpick = dwide %>% filter( ! is.na(logrug_90), ! is.na(total_cover_90)  )
+# 
+# # read estimates of community growth (units = percent cover per day)
+# slopes <- read_csv("data/cover_rate_slopes.csv")
+# 
+# # merge
+# dpick <- left_join( dpick, slopes )
+# write to disk
+# write_csv(dpick, "data/data_sem.csv")
 
-# read estimates of community growth (units = percent cover per day)
-slopes <- read_csv("data/cover_rate_slopes.csv")
+# -----
 
-# merge
-dpick <- left_join( dpick, slopes )
-
+# load merged data
+d <- read_csv("data/data_sem.csv")
 
 ### Structural Equation Modeling
 ## compare two models, each with the same number of degrees of freedom, but different directionality
@@ -38,13 +44,11 @@ dpick <- left_join( dpick, slopes )
 ## Structural equation modeling
 
 # link to path diagrams <https://app.diagrams.net/?src=about#Hmawhal%2Fpanels-complexity%2Fmain%2Fsem%2FPanels%20SEM.drawio#%7B%22pageId%22%3A%22D_jNqRS2Lb4KAGGym6pT%22%7D>
-# also found in "sem/Panels SEM.drawio"
+# also found in "sem/Panels SEM.drawio" in this project
 
 
-
-
-# create SEM from individual models
-# SEM1 - diversity influences complexity
+# create SEM using lavaan
+##### SEM1 - diversity influences complexity
 sem1 <- '
   # regressions
   estimate ~ temp_mean + sal_mean
@@ -63,10 +67,10 @@ sem1 <- '
   ar_bryo_30 ~~ ar_bryo_30
   # covariances of residuals
 '
-fit1 <- lavaan(sem1, data = dpick)
+fit1 <- lavaan(sem1, data = d)
 summary(fit1, fit.measures = T, standardized = T, rsquare = T)
 
-# SEM2 - complexity influences diversity
+##### SEM2 - complexity influences diversity
 sem2 <- '
   # regressions
   estimate ~ temp_mean + sal_mean
@@ -85,7 +89,7 @@ sem2 <- '
   ar_bryo_30 ~~ ar_bryo_30
   # covariances of residuals
 '
-fit2 <- lavaan(sem2, data = dpick)
+fit2 <- lavaan(sem2, data = d)
 summary(fit2, fit.measures = T, standardized = T, rsquare = T)
 tidySEM::graph_sem(fit2)
 #
@@ -104,10 +108,10 @@ nonnest2::vuongtest( fit1, fit2, nested = FALSE )
 # Piecewise SEM
 # SEM1 - diversity influences complexity
 psem1 <- psem(
-  lm( estimate ~ temp_mean + sal_mean, data = dpick),
-  lm( richness_30 ~ temp_mean + sal_mean + estimate, data = dpick),
-  lm( logrug_90 ~ temp_mean + estimate + richness_30 + ar_bryo_30, data = dpick),
-  lm( ar_bryo_30 ~  temp_mean + sal_mean, data = dpick)
+  lm( estimate ~ temp_mean + sal_mean, data = d),
+  lm( richness_30 ~ temp_mean + sal_mean + estimate, data = d),
+  lm( logrug_90 ~ temp_mean + estimate + richness_30 + ar_bryo_30, data = d),
+  lm( ar_bryo_30 ~  temp_mean + sal_mean, data = d)
 )
 summary(psem1)
 basisSet(psem1)
@@ -115,10 +119,10 @@ dSep(psem1)
 
 # SEM2 - complexity influences diversity
 psem2 <- psem(
-  lm( estimate ~ temp_mean + sal_mean , data = dpick),
-  lm( richness_90 ~ temp_mean + sal_mean + estimate + logrug_30, data = dpick),
-  lm( logrug_30 ~  estimate + ar_bryo_30, data = dpick),
-  lm( ar_bryo_30 ~  temp_mean + sal_mean, data = dpick)
+  lm( estimate ~ temp_mean + sal_mean , data = d),
+  lm( richness_90 ~ temp_mean + sal_mean + estimate + logrug_30, data = d),
+  lm( logrug_30 ~  estimate + ar_bryo_30, data = d),
+  lm( ar_bryo_30 ~  temp_mean + sal_mean, data = d)
   )
 summary(psem2)
 basisSet(psem2)
